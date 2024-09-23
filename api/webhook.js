3beb4c5295689b8fbb41258f9796c8e1919f95c0
Xecsa <xecsaba@gmail.com>
2024-09-23 10:53:09 +0400

    developer id updated

--- api/webhook.js
+++ api/webhook.js
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
           create_item (
             board_id: ${boardId},
             item_name: "${commit.message}",
-            column_values: "{\\"author\\": \\"${commit.author}\\", \\"commit_url\\": \\"${commit.url}\\", \\"timestamp\\": \\"${commit.timestamp}\\", \\"repository\\": \\"${commit.repository}\\"}"
+            column_values: "{\\"text4__1\\": \\"${commit.author}\\", \\"commit_url\\": \\"${commit.url}\\", \\"timestamp\\": \\"${commit.timestamp}\\", \\"repository\\": \\"${commit.repository}\\"}"
           ) {
             id
           }
