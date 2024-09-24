6748f6b7d2b67868a1ce9d05640ee0da1b6a93f0
Xecsa <xecsaba@gmail.com>
2024-09-24 15:23:21 +0400

    Date update

--- api/webhook.js
+++ api/webhook.js
@@ -43,7 +43,8 @@ export default async function handler(req, res) {
     // Loop through each commit and send it as an item to the Monday.com board
     for (const commit of commits) {
 
-      const formattedTimestamp = commit.timestamp; 
+      const formattedTimestamp = commit.timestamp.split('T')[0]; // Only date: YYYY-MM-DD
+
       
       const query = `
         mutation {
